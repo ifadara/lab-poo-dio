e7ec7581485ec602573a5594dc539e43816e05ad
Icaro Silva <dsicejay@gmail.com>
2024-05-18 22:22:13 -0300

    Mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import br.com.dio.desafio.domain.Curso;
+import br.com.dio.desafio.domain.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setTitulo("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso js");
+        curso2.setTitulo("Descrição curso js");
+        curso2.setCargaHoraria(4);
+
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescricao("Descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        System.out.println(mentoria);
+
+
     }
 }
